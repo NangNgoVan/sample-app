9c2d6d02312d287e8c707faf2f907e2105bcfe97
Nang Ngo <nangnv2641@gmail.com>
2017-07-05 22:11:14 +0700

    chap-10

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,6 +8,7 @@ module SessionsHelper
       @current_user ||= User.find_by id: user_id
     elsif user_id = cookies.signed[:user_id]
       user = User.find_by id: user_id
+
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
         @current_user = user
@@ -19,6 +20,10 @@ module SessionsHelper
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def current_user? user
+    user == current_user
   end
 
   def logged_in?
@@ -34,5 +39,14 @@ module SessionsHelper
   def log_out
     session.delete :user_id
     @current_user = nil
+  end
+
+  def redirect_back_or default
+    redirect_to session[:forwarding_url] || default
+    session.delete :forwarding_url
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
